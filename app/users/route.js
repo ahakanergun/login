1499b47608df7df40200636fd8edbe294f2e73cb
ahmet <ahakanergun@gmail.com>
2016-01-26 17:52:16 +0200

    form ve sayfalar eklendi

---
+++ app/users/route.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [{
+      name: "Ali Veli",
+      age: "24"
+    }, {
+      name: "Ahmet Mehmet",
+      age: "41"
+    }, {
+      name: "Hasan Hüseyin",
+      age: "18"
+    }];
+  }
+});
