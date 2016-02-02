33b2901349c95535669f8409576f6c06256e673b
ahmet <ahakanergun@gmail.com>
2016-02-02 14:35:00 +0200

    liqud disabled

--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,6 @@
 export default function(){
   this.transition(
-    this.fromRoute('user.new'),
+    this.fromRoute('index'),
     this.toRoute('users'),
     this.use('toLeft'),
     this.reverse('toRight')
